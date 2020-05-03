9bf78aa1acfe14d9eaa0f9453d9df5e02198a551
leahtopchik <leah.topchik@gmail.com>
2020-05-03 18:10:30 +0100

    feat: add test for creating and retrieving image

--- api/progimage/tests.py
+++ api/progimage/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+import pytest
+import base64
 
-# Create your tests here.
+from io import BytesIO
+from PIL import Image as img
+
+from .models import Image
+
+
+@pytest.fixture
+def image_base64():
+    file = BytesIO()
+    image = img.new('RGBA', size=(50, 50), color=(155, 0, 0))
+    image.save(file, 'png')
+    file.name = 'test.png'
+    file.seek(0)
+    return base64.b64encode(file.read()).decode('utf-8')
+
+
+@pytest.mark.django_db
+def test_image(client, image_base64):
+    response = client.post(
+        '/progimage/images/',
+        {'image': image_base64},
+        format='json'
+    )
+    
+    assert response.status_code == 201
+    assert Image.objects.count() == 1
+    
+    response_data = response.json()
+    assert 'id' in response_data.keys()
+
+    id = response_data['id']
+    response = client.get(
+        f'/progimage/images/{id}/'
+    )
+
+    assert response.status_code == 200
+    
+    response_data = response.json()
+    assert response_data['id'] == id
